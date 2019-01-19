8a4e814fa72effec6b6ce76794fec37362dee232
ajluque <ajluque@iesfranciscodelosrios.es>
2019-01-19 13:14:28 +0100

    Added slides to the home activity

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {IonSlides} from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+    @ViewChild('SwipedTabsSlider') SwipedTabsSlider: IonSlides;
 
+    SwipedTabsIndicator: any = null;
+    tabs = ['selectTab(0)', 'selectTab(1)', 'selectTab(2)'];
+    public category: any = '0';
+    ntabs = 3;
+
+    constructor() {}
+
+    ionViewDidEnter() {
+        this.SwipedTabsIndicator = document.getElementById('indicator');
+    }
+
+    /* Actualiza la categoría que esté en ese momento activa*/
+    updateCat(cat: Promise<any>) {
+        cat.then(dat => {
+            this.category = dat;
+            this.category = +this.category;
+        });
+    }
+
+    /* El método que permite actualizar el indicado cuando se cambia de slide*/
+    updateIndicatorPosition() {
+        this.SwipedTabsSlider.getActiveIndex().then(i => {
+            if (this.ntabs > i) {  // this condition is to avoid passing to incorrect index
+                this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (i * 100) + '%,0,0)';
+            }
+        });
+    }
+
+    /* El método que anima la "rayita" mientras nos estamos deslizando por el slide*/
+    animateIndicator(e) {
+        // console.log(e.target.swiper.progress);
+        if (this.SwipedTabsIndicator) {
+            this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' +
+                ((e.target.swiper.progress * (this.ntabs - 1)) * 100) + '%,0,0)';
+        }
+    }
 }
